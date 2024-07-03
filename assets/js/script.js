c9e0a7a40148c3cebcfe1b4744801624f743acb9
wepopagani <wepo.pagani@gmail.com>
2024-07-03 19:16:19 +0200

    not sure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,184 +1,127 @@
 /* ========================================================================= */
-/*	Page Preloader
+/* Page Preloader
 /* ========================================================================= */
-
 $(window).on('load', function () {
-	$('.preloader').fadeOut(100);
+    $('.preloader').fadeOut(100);
 });
 
 jQuery(function ($) {
-	"use strict";
+    "use strict";
 
-	/* ========================================================================= */
-	/*	lazy load initialize
-	/* ========================================================================= */
+    /* ========================================================================= */
+    /* lazy load initialize
+    /* ========================================================================= */
+    const observer = lozad(); // lazy loads elements with default selector as ".lozad"
+    observer.observe();
 
-	const observer = lozad(); // lazy loads elements with default selector as ".lozad"
-	observer.observe();
+    /* ========================================================================= */
+    /* Magnific popup
+    /* ========================================================================= */
+    $('.image-popup').magnificPopup({
+        type: 'image',
+        removalDelay: 160, //delay removal by X to allow out-animation
+        callbacks: {
+            beforeOpen: function () {
+                // just a hack that adds mfp-anim class to markup
+                this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+                this.st.mainClass = this.st.el.attr('data-effect');
+            }
+        },
+        closeOnContentClick: true,
+        midClick: true,
+        fixedContentPos: false,
+        fixedBgPos: true
+    });
 
-	/* ========================================================================= */
-	/*	Magnific popup
-	/* =========================================================================  */
-	$('.image-popup').magnificPopup({
-		type: 'image',
-		removalDelay: 160, //delay removal by X to allow out-animation
-		callbacks: {
-			beforeOpen: function () {
-				// just a hack that adds mfp-anim class to markup
-				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-				this.st.mainClass = this.st.el.attr('data-effect');
-			}
-		},
-		closeOnContentClick: true,
-		midClick: true,
-		fixedContentPos: false,
-		fixedBgPos: true
-	});
+    /* ========================================================================= */
+    /* animation scroll js
+    /* ========================================================================= */
+    function myFunction(x) {
+        if (x.matches) {
+            var topOf = 50
+        } else {
+            var topOf = 350
+        }
+    }
 
-	/* ========================================================================= */
-	/*	Portfolio Filtering Hook
-	/* =========================================================================  */
+    var html_body = $('html, body');
+    $('nav a, .page-scroll').on('click', function () { //use page-scroll class in any HTML tag for scrolling
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 50
+                }, 1500, 'easeInOutExpo');
+                return false;
+            }
+        }
+    });
 
-	var containerEl = document.querySelector('.shuffle-wrapper');
-	if (containerEl) {
-		var Shuffle = window.Shuffle;
-		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-			itemSelector: '.shuffle-item',
-			buffer: 1
-		});
+    // easeInOutExpo Declaration
+    jQuery.extend(jQuery.easing, {
+        easeInOutExpo: function (x, t, b, c, d) {
+            if (t === 0) {
+                return b;
+            }
+            if (t === d) {
+                return b + c;
+            }
+            if ((t /= d / 2) < 1) {
+                return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+            }
+            return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+        }
+    });
 
-		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-			var input = evt.currentTarget;
-			if (input.checked) {
-				myShuffle.filter(input.value);
-			}
-		});
-	}
+    /* ========================================================================= */
+    /* counter up
+    /* ========================================================================= */
+    function counter() {
+        var oTop;
+        if ($('.count').length !== 0) {
+            oTop = $('.count').offset().top - window.innerHeight;
+        }
+        if ($(window).scrollTop() > oTop) {
+            $('.count').each(function () {
+                var $this = $(this),
+                    countTo = $this.attr('data-count');
+                $({
+                    countNum: $this.text()
+                }).animate({
+                    countNum: countTo
+                }, {
+                    duration: 1000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        $this.text(this.countNum);
+                    }
+                });
+            });
+        }
+    }
+    $(window).on('scroll', function () {
+        counter();
+    });
 
-	/* ========================================================================= */
-	/*	Testimonial Carousel
-	/* =========================================================================  */
+    /* ========================================================================= */
+    /* Contact Form Submission Handling
+    /* ========================================================================= */
+    document.addEventListener("DOMContentLoaded", function () {
+        const form = document.getElementById("contact-form");
+        const successMessage = document.getElementById("success-message");
+        const errorMessage = document.getElementById("error-message");
 
-	$("#testimonials").slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 4000
-	});
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
 
-	/* ========================================================================= */
-	/*	animation scroll js
-	/* ========================================================================= */
-
-
-
-	function myFunction(x) {
-		if (x.matches) {
-			var topOf = 50
-		} else {
-			var topOf = 350
-		}
-	}
-
-	var html_body = $('html, body');
-	$('nav a, .page-scroll').on('click', function () { //use page-scroll class in any HTML tag for scrolling
-		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				html_body.animate({
-					scrollTop: target.offset().top - 50
-				}, 1500, 'easeInOutExpo');
-				return false;
-			}
-		}
-	});
-
-	// easeInOutExpo Declaration
-	jQuery.extend(jQuery.easing, {
-		easeInOutExpo: function (x, t, b, c, d) {
-			if (t === 0) {
-				return b;
-			}
-			if (t === d) {
-				return b + c;
-			}
-			if ((t /= d / 2) < 1) {
-				return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
-			}
-			return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
-		}
-	});
-
-	/* ========================================================================= */
-	/*	counter up
-	/* ========================================================================= */
-	function counter() {
-		var oTop;
-		if ($('.count').length !== 0) {
-			oTop = $('.count').offset().top - window.innerHeight;
-		}
-		if ($(window).scrollTop() > oTop) {
-			$('.count').each(function () {
-				var $this = $(this),
-					countTo = $this.attr('data-count');
-				$({
-					countNum: $this.text()
-				}).animate({
-					countNum: countTo
-				}, {
-					duration: 1000,
-					easing: 'swing',
-					step: function () {
-						$this.text(Math.floor(this.countNum));
-					},
-					complete: function () {
-						$this.text(this.countNum);
-					}
-				});
-			});
-		}
-	}
-	$(window).on('scroll', function () {
-		counter();
-	});
-
-	/* ========================================================================= */
-	/*	Contact Form Submission Handling
-	/* ========================================================================= */
-	document.addEventListener("DOMContentLoaded", function () {
-		const form = document.getElementById("contact-form");
-		const successMessage = document.getElementById("success-message");
-		const errorMessage = document.getElementById("error-message");
-	
-		form.addEventListener("submit", function (event) {
-			event.preventDefault();
-	
-			const formData = new FormData(form);
-	
-			fetch("/", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded",
-				},
-				body: new URLSearchParams(formData).toString(),
-			})
-				.then((response) => {
-					if (response.ok) {
-						form.reset();
-						successMessage.style.display = "block";
-						errorMessage.style.display = "none";
-					} else {
-						throw new Error("Network response was not ok");
-					}
-				})
-				.catch((error) => {
-					console.error("There was a problem with your submission:", error);
-					successMessage.style.display = "none";
-					errorMessage.style.display = "block";
-				});
-		});
-	});
-	
-
+			form.reset()
+			successMessage.style.display = "block";
+			errorMessage.style.display = "none";
+            
+        });
+    });
 });
